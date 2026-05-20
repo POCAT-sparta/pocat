6e944609d596fffe13f6d25fa87e7be1babeb120
Taeguni06 <aqwsde0412@gmail.com>
2026-05-20 16:18:16 +0900

    Update CardController.java

--- src/main/java/com/rocketcrew/pocat/domain/card/controller/CardController.java
+++ src/main/java/com/rocketcrew/pocat/domain/card/controller/CardController.java
@@ -37,9 +37,9 @@ public class CardController {
     public ResponseEntity<ApiResponseDto<PageResponseDto<CardResponse>>> getCards(
             @RequestParam(required = false) String keyword,
             @RequestParam(required = false) String setName,
+            @RequestParam(required = false) String series,
             @RequestParam(required = false) CardGrade grade,
             @RequestParam(required = false) CardCategory category,
-            @RequestParam(required = false) String series,
             @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         CardSearchCondition condition = new CardSearchCondition(keyword, setName, grade, category, CardStatus.ACTIVE);
         Page<CardResponse> page = cardQueryService.getCards(condition, pageable);
