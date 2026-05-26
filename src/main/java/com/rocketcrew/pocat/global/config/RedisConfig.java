e79a8f2cc97b193a8d525138f11a8e7457f75f9d
Max-1012 <thdud001012@gmail.com>
2026-05-26 15:34:58 +0900

    feat: Redis 만료 이벤트 기반 경매 종료 처리 추가

--- src/main/java/com/rocketcrew/pocat/global/config/RedisConfig.java
+++ src/main/java/com/rocketcrew/pocat/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.rocketcrew.pocat.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import com.rocketcrew.pocat.domain.auction.service.AuctionExpirationRedisSubscriber;
 import com.rocketcrew.pocat.domain.notification.service.NotificationRedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -48,10 +49,12 @@ public class RedisConfig {
     @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(
             RedisConnectionFactory connectionFactory,
-            NotificationRedisSubscriber subscriber) {
+            NotificationRedisSubscriber subscriber,
+            AuctionExpirationRedisSubscriber auctionExpirationRedisSubscriber) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.addMessageListener(subscriber, new PatternTopic("notification:*"));
+        container.addMessageListener(auctionExpirationRedisSubscriber, new PatternTopic("__keyevent@*__:expired"));
         return container;
     }
 
--- src/main/java/com/rocketcrew/pocat/global/exception/common/ErrorCode.java
+++ src/main/java/com/rocketcrew/pocat/global/exception/common/ErrorCode.java
@@ -39,6 +39,7 @@ public enum ErrorCode {
     AUCTION_REASON_REQUIRED(HttpStatus.BAD_REQUEST, "사유를 입력해주세요."),
     AUCTION_CANNOT_CANCEL(HttpStatus.CONFLICT, "취소할 수 없는 경매 상태입니다."),
     AUCTION_LOCK_FAILED(HttpStatus.CONFLICT, "다른 경매 처리가 진행 중입니다. 잠시 후 다시 시도해주세요."),
+    AUCTION_INVALID_STATUS_TRANSITION(HttpStatus.CONFLICT, "변경할 수 없는 경매 상태입니다."),
 
     // Bid
     BID_NOT_FOUND(HttpStatus.NOT_FOUND, "입찰을 찾을 수 없습니다."),
@@ -49,6 +50,7 @@ public enum ErrorCode {
     BID_PRICE_TOO_LOW(HttpStatus.CONFLICT, "입찰가는 최소 입찰가와 현재 최고가보다 높아야 합니다."),
     BID_BUYOUT_PRICE_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "즉시구매가 이상 금액은 즉시구매 API를 호출해야 합니다."),
     BID_ALREADY_LEADING(HttpStatus.CONFLICT, "이미 현재 최고 입찰자입니다."),
+    BID_INVALID_STATUS_TRANSITION(HttpStatus.CONFLICT, "변경할 수 없는 입찰 상태입니다."),
 
     // Order
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
