b057ca80f3db49ebd6a45832521a350ccbfb435c
tmdgsudl <leesh27955@gmail.com>
2026-05-29 16:38:03 +0900

    refactor: sql 수정.....

--- src/main/resources/db/migration/V6__add_order_type.sql
+++ src/main/resources/db/migration/V6__add_order_type.sql
@@ -1,1 +1,4 @@
-ALTER TABLE orders ADD COLUMN order_type VARCHAR(20) NOT NULL DEFAULT 'AUCTION';
+ALTER TABLE orders
+    ADD COLUMN order_type VARCHAR(20) NOT NULL DEFAULT 'AUCTION',
+    ADD CONSTRAINT chk_orders_order_type
+        CHECK (order_type IN ('AUCTION', 'BUYOUT'));
