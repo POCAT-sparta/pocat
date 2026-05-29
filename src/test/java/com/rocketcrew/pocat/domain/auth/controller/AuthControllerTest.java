427bca7337ca4bdd0ca207656edffc603cef7ce6
cdkkyj123 <cdkkyj123@gmail.com>
2026-05-29 09:17:17 +0900

    fix(#133): AuthControllerTest UnnecessaryStubbingException — LENIENT 설정 추가

--- src/test/java/com/rocketcrew/pocat/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/com/rocketcrew/pocat/domain/auth/controller/AuthControllerTest.java
@@ -20,6 +20,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -37,6 +39,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class AuthControllerTest {
 
     private MockMvc mockMvc;
